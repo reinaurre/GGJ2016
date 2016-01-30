1620d81f4e93fdbc44fb9af4c1d96497c745c30c
Robert Ramsay <Robert Ramsay>
2016-01-29 23:19:30 -0800

    Initial Folder Structure checkin

---
+++ Habitual Sacrifices/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour
+{
+    private float _score;
+    public float Score { get; set; }
+
+    private int _lives;
+    public int Lives { get; set; }
+
+
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    
+	}
+
+}
