146a72cbdb2317755a233fce7f0beb59ad95fd60
chaosed0 <chaosed0@gmail.com>
2016-01-30 12:37:00 -0800

    Implement new life display

--- Habitual Sacrifices/Assets/_Scripts/ui/Hud.cs
+++ Habitual Sacrifices/Assets/_Scripts/ui/Hud.cs
@@ -5,15 +5,15 @@ class Hud : MonoBehaviour {
     public Text scoreText = null;
     public Text timerText = null;
 
-    public Image[] lives = null;
-    public Sprite aliveSprite = null;
-    public Sprite deadSprite = null;
+    public Image[] deaths = null;
 
     void Start() {
         Object.DontDestroyOnLoad(this.gameObject);
 
-        for (int i = 0; i < lives.Length; i++) {
-            lives[i].sprite = aliveSprite;
+        for (int i = 0; i < deaths.Length; i++) {
+            Color color = deaths[i].color;
+            color.a = 0.0f;
+            deaths[i].color = color;
         }
     }
 
@@ -24,11 +24,15 @@ class Hud : MonoBehaviour {
         timerText.text = string.Format("{0:0.00}",
                 manager.ModifiedLevelTime - manager.LevelTimer).Replace(".", ":");
 
-        for (int i = 0; i < lives.Length; i++) {
-            if (i < manager.Lives) {
-                lives[i].sprite = aliveSprite;
+        for (int i = 0; i < deaths.Length; i++) {
+            if (i >= manager.Lives) {
+                Color color = deaths[i].color;
+                color.a = 1.0f;
+                deaths[i].color = color;
             } else {
-                lives[i].sprite = deadSprite;
+                Color color = deaths[i].color;
+                color.a = 0.0f;
+                deaths[i].color = color;
             }
         }
     }
