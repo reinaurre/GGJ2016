366b2dec9da948a50421be5c285d052595e397fe
chaosed0 <chaosed0@gmail.com>
2016-01-30 11:13:48 -0800

    Get started on the life counter

--- Habitual Sacrifices/Assets/_Scripts/ui/Hud.cs
+++ Habitual Sacrifices/Assets/_Scripts/ui/Hud.cs
@@ -5,15 +5,31 @@ class Hud : MonoBehaviour {
     public Text scoreText = null;
     public Text timerText = null;
 
+    public Image[] lives = null;
+    public Sprite aliveSprite = null;
+    public Sprite deadSprite = null;
+
     void Start() {
         Object.DontDestroyOnLoad(this.gameObject);
+
+        for (int i = 0; i < lives.Length; i++) {
+            lives[i].sprite = aliveSprite;
+        }
     }
 
     void Update() {
-        GameManager gameManager = ServiceLocator.GetGameManager();
+        GameManager manager = ServiceLocator.GetGameManager();
 
-        scoreText.text = string.Format("{0}", (int)gameManager.Score);
+        scoreText.text = string.Format("{0}", (int)manager.Score);
         timerText.text = string.Format("{0:0.00}",
-                gameManager.ModifiedLevelTime - gameManager.LevelTimer).Replace(".", ":");
+                manager.ModifiedLevelTime - manager.LevelTimer).Replace(".", ":");
+
+        for (int i = 0; i < lives.Length; i++) {
+            if (i < manager.Lives) {
+                lives[i].sprite = aliveSprite;
+            } else {
+                lives[i].sprite = deadSprite;
+            }
+        }
     }
 }
