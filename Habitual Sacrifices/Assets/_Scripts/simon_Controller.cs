0f16593412aca579c4cb870c630296b08ac83c4d
shad0wth3iffury <shad0wth3iffury@hotmail.com>
2016-01-29 23:36:51 -0800

    Sean initial commit

---
+++ Habitual Sacrifices/Assets/_Scripts/simon_Controller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class simon_Controller : MonoBehaviour {
+
+	public GameObject objectCam;
+	public GameObject objectCell;
+
+	Animator objectAnim;
+	Animator objectCamAnim;
+
+	// Use this for initialization
+	void Start () {
+	
+		objectAnim = objectCell.GetComponent <Animator> ();
+		objectCamAnim = objectCam.GetComponent <Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (Input.GetButtonDown ("Fire1")) {
+			objectAnim.SetTrigger ("blur_rotation");
+			objectCamAnim.SetTrigger ("blur_rotation");
+		}
+
+	}
+}
