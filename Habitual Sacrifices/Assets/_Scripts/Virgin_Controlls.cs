0dee0c0b1a9840b3d7c10b30d49db63a5fe02b26
chaosed0 <chaosed0@gmail.com>
2016-01-30 23:28:18 -0800

    New sacrifice background, sound

--- Habitual Sacrifices/Assets/_Scripts/Virgin_Controlls.cs
+++ Habitual Sacrifices/Assets/_Scripts/Virgin_Controlls.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class Virgin_Controlls : MonoBehaviour {
     public Transform flicker;
+    float prevAxisValue;
     float axisValue;
 
 	// Use this for initialization
@@ -21,11 +22,13 @@ public class Virgin_Controlls : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         axisValue = Input.GetAxis("Horizontal");
-        if(axisValue != 0)
-        {
+        if (Mathf.Abs(prevAxisValue) < 0.3f && Mathf.Abs(axisValue) >= 0.3f) {
             Vector3 rotation = flicker.rotation.eulerAngles;
             rotation.x = 45 * Mathf.Sign(axisValue);
             flicker.rotation = Quaternion.Euler(rotation);
+
+            ServiceLocator.GetSoundSystem().PlaySound("paddle");
         }
+        prevAxisValue = axisValue;
     }
 }
